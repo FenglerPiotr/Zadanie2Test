9e341b3725c8457d57d3d229185298c49398cc69
Piotr Fengler <piotfeng@gmail.com>
2022-06-04 12:18:41 +0200

    Zadanie2

---
+++ src/pl/comarch/szkolenia/test/zadanie2/BinaryTree.java
@@ -0,0 +1,69 @@
+package pl.comarch.szkolenia.test.zadanie2;
+
+import java.util.TreeSet;
+
+//Rozwiązanie odnalezione na https://www.baeldung.com/java-binary-tree wprowadzone zmiany dot metod min i max.
+
+public class BinaryTree {
+    Node root;
+
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            return current;
+        }
+
+        return current;
+    }
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private boolean containsNodeRecursive(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value == current.value) {
+            return true;
+        }
+        return value < current.value
+                ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
+    }
+
+    public boolean containsNode(int value) {
+        return containsNodeRecursive(root, value);
+    }
+    private Node deleteRecursive(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.value) {
+            // Node to delete found
+            // ... code to delete the node will go here
+        }
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+    private int min(Node root) {
+        return root.left == null ? root.value : min(root.left);
+    }
+    private int max(Node root) {
+        return root.right == null ? root.value : max(root.right);
+
+    }
+}
---
+++ src/pl/comarch/szkolenia/test/zadanie2/Node.java
@@ -0,0 +1,14 @@
+package pl.comarch.szkolenia.test.zadanie2;
+
+
+public class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            right = null;
+            left = null;
+        }
+    }
